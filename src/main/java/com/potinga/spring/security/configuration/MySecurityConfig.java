c8f9c2605ebed6dd70f89bcd25c8f71570b1b63a
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-10-08 14:47:55 +0300

    Create MySecurityConfig.java

---
+++ src/main/java/com/potinga/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,19 @@
+package com.potinga.spring.security.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("potinga").password("potinga").roles("Employee"))
+                .withUser(userBuilder.username("elena").password("elena").roles("HR"))
+                .withUser(userBuilder.username("ivan").password("ivan").roles("Menager", "HR"));
+    }
+}
