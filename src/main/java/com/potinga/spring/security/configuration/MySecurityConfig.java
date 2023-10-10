dd7a0d8e06152221b37a4d30b1ff425db504082a
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-10-10 18:19:33 +0300

    Update MySecurityConfig.java

--- src/main/java/com/potinga/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/potinga/spring/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.potinga.spring.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -12,8 +13,17 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         UserBuilder userBuilder = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("potinga").password("potinga").roles("Employee"))
+                .withUser(userBuilder.username("potinga").password("potinga").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("elena").password("elena").roles("HR"))
-                .withUser(userBuilder.username("ivan").password("ivan").roles("Menager", "HR"));
+                .withUser(userBuilder.username("ivan").password("ivan").roles("MENAGER", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MENAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/menager_info").hasRole("MENAGER")
+                .and().formLogin().permitAll();
     }
 }
