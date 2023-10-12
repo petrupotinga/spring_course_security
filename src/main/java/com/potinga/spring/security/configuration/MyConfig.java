b780c8e6ac137dfb16c79c442398a1e8bbd49a6c
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-10-12 19:01:53 +0300

    Update MyConfig.java

--- src/main/java/com/potinga/spring/security/configuration/MyConfig.java
+++ src/main/java/com/potinga/spring/security/configuration/MyConfig.java
@@ -1,11 +1,15 @@
 package com.potinga.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "com.potinga.spring.security")
@@ -18,5 +22,18 @@ public class MyConfig {
         internalResourceViewResolver.setPrefix("/WEB-INF/view/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
+    }
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("root");
+            dataSource.setPassword("root");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
     }
 }
